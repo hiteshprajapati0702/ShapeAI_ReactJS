b146e069c555981f5539e001e1e8534c24cae65b
hiteshprajapati0702 <86594969+hiteshprajapati0702@users.noreply.github.com>
2021-06-28 13:47:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Info from "./Info";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>7 DAYS BOOTCAMP</h1>
+        <p>Javascript And React JS Bootcamp By ShapeAI...</p>
+      </div>
+      <div className="note">
+        <h1>7 DAYS BOOTCAMP</h1>
+        <p>Javascript And Node JS Bootcamp By ShapeAI...</p>
+      </div>{" "}
+      <div className="note">
+        <h1>7 DAYS BOOTCAMP</h1>
+        <p>Python and Cyber Security Bootcamp By ShapeAI...</p>
+      </div>
+      <div className="note1">
+        <p> 7 Days Training and Bootcamps info...</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Info;
